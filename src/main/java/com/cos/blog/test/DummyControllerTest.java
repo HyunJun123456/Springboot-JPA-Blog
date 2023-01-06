7b612993716ab72e2751aea11fb9450247d64016
HyunJun123456 <juni303@chauniv.ac.kr>
2023-01-06 22:03:01 +0900

    삭제하기 추가

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,11 +1,22 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +30,58 @@ public class DummyControllerTest {
 	
 	@Autowired // 메모리에 같이 띄워줌. 의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+		}
+		return "삭제되었습니다. id: "+id;
+	}
+	
+	
+	// email, password 수정.
+	@Transactional // 함수 종료시에 자동 commit 됨.
+	@PutMapping("/dummy/user/{id}") // 알아서 매핑값을 구분해줌 put, get 등
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // json 데이터를 요청 => Java Object를 messageConverter가 Jackson 라이브러리로 변환해서 받아줌.
+		System.out.println("id: "+id);
+		System.out.println("password: "+requestUser.getPassword());
+		System.out.println("email: "+requestUser.getEmail());
+		User user = userRepository.findById(id).orElseThrow(()->{// 익명 함수로 파라미터에 함수가 들어갈 수 있음.
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		//userRepository.save(requestUser); 
+		// save 함수는 id를 전달하지 않으면 insert를 해주고
+		// save 함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+		// save 함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 함.
+		
+		// 더티 체킹
+		return user;
+	}
+	
+	
+	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// 한페이지당 2건에 데이터를 리턴받아 볼 예정.
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
+		
+		Page<User> pagingUser =  userRepository.findAll(pageable); // 페이징 하는 방법
+		if(pagingUser.isFirst()) {
+			
+		}
+		List<User> users =  userRepository.findAll(pageable).getContent();
+		return users;
+	}
 	
 	// {id} 주소로 파라메터를 전달 받을 수 있음.
 	// http://localhost:8000/blog/dummy/user/3
