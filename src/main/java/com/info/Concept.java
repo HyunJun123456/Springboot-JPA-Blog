204241e00c2c655c75b4b3f0cae073e1a2c80a03
HyunJun123456 <juni303@chauniv.ac.kr>
2023-01-12 21:57:49 +0900

    정보처리기사 공부중

---
+++ src/main/java/com/info/Concept.java
@@ -0,0 +1,62 @@
+package com.info;
+
+public class Concept {
+	/*
+	시스템의 기본 요소
+	- 입력, 처리, 출력, 제어, 피드백으로 구성
+	
+	CASE의 분류
+	상위(Upper) CASE: 요구분석 및 설계 단계 지원 (모델 간 모순 검사 기능, 모델 오류 검증 가능, 자료 흐름도 작성 기능)
+	하위(Lower) CASE: 소스 코드 작성, 테스트, 문서화 과정 지원
+	통합(Integrate) CASE: 소프트웨어 개발 주기 전체 과정 지원
+	
+	요구사항 분석을 위한 CASE 도구
+	SADT(Structured Analysis and Design Technique)
+	: 블록 다이어그램을 채택한 자동화 도구다.
+	
+	나선형 모형(Spiral Model)
+	계획 수립 -> 위험 분석 -> 개발 및 검증 -> 고객 평가
+	
+	HIPO(Hierarchy Input Process Output)
+	- 가시적 도표, 총체적 다이어그램, 세부적 다이어그램으로 구성
+	
+	V-모델
+	- 폭포수 모형에 시스템 검증과 테스트 작업을 강조한 모델
+	
+	애자일(Agile) 개발 방법론
+	종류
+	: 익스트림프로그래밍(XP, eXtremeProgramming)
+	, 스크럼(SCRUM)
+	, 린(Lean)
+	, DSDM(Dynamic System Development Method, 동적 시스템 개발 방법론)
+	, FDD(Feature Driven Development, 기능 중심 개발)
+	
+	XP의 12가지 실천사항(Practice)
+	- Fine Scale Feedback
+		- Pair Programming(짝 프로그래밍): 두 사람이 짝이 되어 한 사람은 코딩을 다른 사람은 검사를 수행하는 방식이다.
+		- Planning Game: 게임처럼 선수와 규칙, 목표를 두고 기획에 임한다.
+		- Test Driven Development: 실제 코드를 작성하기 전에 단위 테스트부터 작성 및 수행하며, 이를 기반으로 코드를 작성한다.	
+		- Whole Team: 개발 효율을 위해 고객을 프로젝트 팀원으로 상주시킨다.
+		
+	- Continuous Process
+		- Continuous Integration: 상시 빌드 및 배포를 할 수 있는 상태로 유지한다.
+		- Design Improvement: 기능 변경 없이 중복성/복잡성 제거, 커뮤니케이션 향상, 단순화, 유연성 등을 위한 재구성을 수행한다.
+		- Small Releases: 짧은 주기로 잦은 릴리즈를 함으로써 고객이 변경사항을 볼 수 있게 한다.
+		
+	- Shared Understanding
+		- Coding Standards: 소스 코드 작성 포맷과 규칙들을 표준화된 관레에 따라 작성한다.
+		- Collective Code Ownership: 시스템에 있는 소스 코드는 팀의 모든 프로그래머가 누구든지 언제라도 수정할 수 있다.
+		- Simple Design: 가능한 가장 간결한 디자인 상태를 유지한다.
+		- System Metaphor: 최종적으로 개발되어야 할 시스템의 구조를 기술한다.
+		
+	- Programmer Welfare
+		- Sustainable Pace: 일주일에 40시간 이상 작업 금지, 2주 연속 오버타임을 금지한다.
+		
+	============================================================================
+	
+	
+	
+	
+	
+	*/
+}
